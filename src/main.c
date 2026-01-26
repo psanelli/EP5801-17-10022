477eb0d2f6d8282403fe7563add430c1bde1acf9
Paola Anelli <panelli@rintronic.com>
2026-01-26 08:09:37 -0400

    Hello World

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// Paola Anelli
+// 17 - 10022
+// EP5801 - Programacion Avanzada en C de Bajo Nivel
+// 26 de Enero de 2026
+// Hello World in C
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
